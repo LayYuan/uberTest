5af2d926ded32ea79c6f02be5522ba766aab98a2
Yuan25 <chuahlayyuan@gmail.com>
2018-04-04 20:03:07 +0800

    1) Add in Facebase auth. 2) UI Connection.

--- uberTest/ViewController.swift
+++ uberTest/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var driverLabel: UILabel!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var signUpMode = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func topTapped(_ sender: Any) {
     }
-
-
+    
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        
+        if signUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign UP", for: .normal)
+            riderDriverSwitch.isHidden = true
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            signUpMode = false
+        }else {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderDriverSwitch.isHidden = false
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            signUpMode = true
+        }
+    }
+    
 }
 
